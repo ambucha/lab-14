2b0d66c7d41dd12a44205621fc7d3aa4647209cb
ambucha <ambuchatskiy@gmail.com>
2025-09-27 12:43:02 -0700

    trhhird commit

--- lab-14.cpp
+++ lab-14.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 // Const int for the separator
-const int SIZE = 10;
+const int SIZE = 20;
 
 // Create the class for colors
 class Color {
@@ -29,8 +29,50 @@ class Color {
 
 // print function definition
 void Color::print() {
+    // gonna create a static int so i can keep track of which color # i am displaying
+    static int count = 0;
+    count++;
+    
+    // Display the information and variables given
+    cout << "Color #" << count << ":" << endl;
     cout << "\tRed: " << red << endl;
     cout << "\tGreen: " << green << endl;
     cout << "\tBlue: " << blue << endl << endl;
-    cout << setfill('-') << setw(SIZE) << endl << endl;
+    cout << setfill('-') << setw(SIZE) << '-' << endl << endl;
+}
+
+int main(){
+    // Define four colors
+    Color crimson;
+    Color gold;
+    Color orchid;
+    Color forestGreen;
+
+    // intialize crimson
+    crimson.setRed(220);
+    crimson.setGreen(20);
+    crimson.setBlue(60);
+
+    //initialize gold
+    gold.setRed(255);
+    gold.setGreen(215);
+    gold.setBlue(0);
+
+    //initialize orchid
+    orchid.setRed(218);
+    orchid.setGreen(112);
+    orchid.setBlue(214);
+
+    // initialize Forest green
+    forestGreen.setRed(34);
+    forestGreen.setGreen(139);
+    forestGreen.setBlue(34);
+
+    // Print the colors
+    crimson.print();
+    gold.print();
+    orchid.print();
+    forestGreen.print();
+
+    return 0;
 }
